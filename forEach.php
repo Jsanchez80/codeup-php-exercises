4d9ada6f0b316b1a36d9fd4686b9fcc3e7d26754
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-08-31 14:12:31 -0500

    Update: Notes are in order and code is cleaned up.

--- forEach.php
+++ forEach.php
@@ -1,11 +1,30 @@
 <?php
+///////////////////////////////////////////////////////////////////////
+// TABLE OF CONTENTS//////////////////////////////////////////////
+//  1. Create a file named forEach.php in your exercise directory. Commit 	each step and push to Github 
+//  2. Construct a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string.
+//  3. Construct a loop that iterates through each value and outputs only values with a type that is scalar.
+//  4. Create a loop that will echo out every value, including those nested in arrays. Output should look like this.
+///////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////
+// ANSWERS: //////////////////////////////////////////////////////
+//  2. ////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+//  3.  In order to accomplish the goal of Question 3, remove all exchos except the echo on line 39.
+////////////////////////////////////////////////////
+//  4.  In order to accomplish the goal of Question 4, remove the "strings" you have in the echo and replace {$thing} with "value" for items containing numeric value.
+// ///////////////////////////////////////////      
+
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
 
 	if (is_array($thing)) {
-		echo "{$thing} is an array" . PHP_EOL;
+		foreach($thing as $value) {
+			echo "$value" . PHP_EOL;
+		}
 }	else if (is_bool($thing)) {
 		echo "{$thing} is boolean" . PHP_EOL;
 }	else if (is_float($thing)) {
@@ -23,9 +42,9 @@ foreach ($things as $thing) {
 }	else if (is_null($thing)) {
 		echo "{$thing} is null" . PHP_EOL;
 }	else if ( is_numeric($thing)) {
-		echo "{$thing} is numeric(a type of float or integer" . PHP_EOL;
+		echo "{$thing} is numeric (a type of float or integer)" . PHP_EOL;
 }	else if (is_scalar($thing)) {
-		echo "{$thing} is scalar (a type of float, integer, string or boolean" . PHP_EOL;
+		echo "{$thing} is scalar (a type of float, integer, string or boolean)" . PHP_EOL;
 } 	else if (is_string($thing)) {
 		echo "{$thing} is a string" . PHP_EOL;
 }
