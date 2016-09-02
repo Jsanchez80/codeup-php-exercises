762107e758d2c3cac80ee87d5794a5b35b45bd0f
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-02 11:02:19 -0500

    Update: Included notes and beginning the solution for question 2.

--- search-arrays.php
+++ search-arrays.php
@@ -1,7 +1,24 @@
 <?php
 
+/////////////////////////////////////////////////////////////////
+// 1. Download and save search-arrays.php to your exercises directory. Commit each step in git and push to GitHub at the end of the exercise.
+///////////////////////////////////////////////////
+
+
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+/////////////////////////////////////////////////////////////////
+// 2. Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
+///////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////////////////
+// 3. Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
+///////////////////////////////////////////////////
+
 
 
