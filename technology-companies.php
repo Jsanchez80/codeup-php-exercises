f94a8dc30df109813de898368dc8556864e00d18
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-02 09:35:31 -0500

    Update: Solved question 2 and printing array in it's current form; working on question 3.

---
+++ technology-companies.php
@@ -0,0 +1,66 @@
+<?php
+
+$companies = [
+    'Sun Microsystems' => [
+        'Vinod Khosla',
+        'Bill Joy',
+        'Andy Bechtolsheim',
+        'Scott McNealy'
+    ],
+    'Silicon Graphics' => [
+        'Jim Clark',
+        'Ed McCracken'
+    ],
+    'Cray' => [
+        'William Norris',
+        'Seymour Cray'
+    ],
+    'NeXT' => [
+        'Steve Jobs',
+        'Avie Tevanian',
+        'Joanna Hoffman',
+        'Bud Tribble',
+        'Susan Kare'
+    ],
+    'Acorn Computers' => [
+        'Steve Furber',
+        'Sophie Wilson',
+        'Hermann Hauser',
+        'Jim Mitchell'
+    ],
+    'MIPS Technologies' => [
+        'Skip Stritter',
+        'John L. Hennessy'
+    ],
+    'Commodore' => [
+        'Yash Terakura',
+        'Bob Russell',
+        'Bob Yannes',
+        'David A. Ziembicki',
+        'Jay Miner'
+    ],
+    'Be Inc' => [
+        'Steve Sakoman',
+        'Jean-Louis Gassée'
+    ]
+];
+
+
+/////////////////////////////////////////////////////////////////
+// 2.Add code to output the $companies array in its current form.
+///////////////////////////////////////////////////
+
+// asort($companies);
+// print_r($companies);
+
+/////////////////////////////////////////////////////////////////
+// 3.Sort the $companies array by company name and output the results.
+///////////////////////////////////////////////////
+
+
+
+
+
+
+
+
