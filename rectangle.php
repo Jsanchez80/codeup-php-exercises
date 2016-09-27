1c14d2ad0a20a928e1ca425c6ef7c5907dc958a8
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-27 15:21:23 -0500

    Initial Commit:

---
+++ rectangle.php
@@ -0,0 +1,38 @@
+<?php
+
+//  Create an instance of a class called Rectangle, set variables for height and width. 
+
+class rectangle
+{
+	public $height;
+	public $width;
+
+	public function __construct($height, $width)
+	{
+		$this->height = $height;
+		$this->width = $width;
+	}
+//  We want to determine the overall area of the object:
+//  area is equal to height multiplied by width. If the sides are uneven in area, 
+	// you might have a rectangle. 
+	public function area()
+	{
+		return ($this->$height * $this->$width);
+		// echo "The height is" . $this->height . "and the width is" . $this->width;
+	} 
+		// area();
+// We want to test whether this is a rectangle or not:
+// 	If the height isn't equal to the width, then you have a rectangle.
+//  If the sides are equal you have a square. 
+	// function isSquare(){
+	// 	if ($this->height == $this $width) {
+	// 		return true;
+	// 		echo "This is a Rectangle";
+	// 	} else {
+	// 		return false;
+	// 		echo "Sorry, you should check your dimensions and try again";
+	// 	}
+	// }
+}
+
+
