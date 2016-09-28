dc26ad90653f33d840b421691ef71bf64f59ff19
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-28 11:13:07 -0500

    Update: Don't remember if I pushed this.

---
+++ shapes_test.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'Rectangle.php';
+
+$rectangle = new Rectangle(7, 5);
+echo "the area is " . $rectangle->area() . PHP_EOL;
+
+
+// $square = new Square(4, 4);
+// echo "The area of this square is " . $square->area() . PHP_EOL; 
+
+
+
+
