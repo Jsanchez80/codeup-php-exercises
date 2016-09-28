ae3ddd4af777e8f36ab2cf66dd6edc52e0a59ac8
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-28 12:20:43 -0500

    Update: Blocked out the function performed in the previous exercise to validate whether data is accessible to the child(Square) when data is protected. Data remains accessible to the child(Square).

--- Square.php
+++ Square.php
@@ -5,7 +5,20 @@ require 'Rectangle.php';
 
  class Square extends Rectangle
 {
-	
+	// public $height;
+	// public $width;
+
+	// public function __construct($height, $width)
+	// {
+	// 	$this->height = $height;
+	// 	$this->width = $width;
+	// }
+
+	// public function area()
+	// {
+	// 	return $this->height * $this->width;
+
+	// } 
 }
 
 
