a1b13ca83e55f73d8edf7e01317d83e1da8a7dc8
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-28 11:42:35 -0500

    Update: I wasn't referencing the files properly.  This prevented the operation of my methods and prompted me with the following error:
    PHP Fatal error:  Class 'Square' not found in /vagrant/exercises/shapes_test.php on line 9
    PHP Stack trace:
    PHP   1. {main}() /vagrant/exercises/shapes_test.php:0
    
    I was searching my code for a syntax error that I may have missed, when I hadn't listed the require_once properly.
    
    	My original reference to the file:
    	require_once 'shapes_test.php';
    
    	Solution for referencing the file:
    	require_once 'Rectangle.php';
    
    As you can see simply referencing the wrong file path can prevent you from receiving the proper output.

--- shapes_test.php
+++ shapes_test.php
@@ -1,14 +1,19 @@
 <?php
+// Remember to link the proper reference points: I was linking Rectangle.php here, which worked when I only had the Rectangle.php to work from. Since I've created a secondary file titled Square.php and trying to reference shapes_test.php from the Square.php file...it wasn't linking up properly.  I literally inverted the two file references and the program operated accordingly. 
 
-require_once 'Rectangle.php';
+require_once 'Square.php';
+
 
 $rectangle = new Rectangle(7, 5);
 echo "the area is " . $rectangle->area() . PHP_EOL;
 
+// My Solution:
+$square = new Square(4, 4);
+echo "The area of this square is " . $square->area() . PHP_EOL; 
 
-// $square = new Square(4, 4);
-// echo "The area of this square is " . $square->area() . PHP_EOL; 
-
-
+// Cams Solution:
+// $square = new Square(5, 5);
+// echo 'The area of the square is ' . $square->area() . PHP_EOL;
+// echo 'The perimeter of the square is ' . $square->perimeter() . PHP_EOL;
 
 
