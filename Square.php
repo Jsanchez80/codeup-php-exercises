95d0213499bfa780fef8d6c7e0ee97781192187b
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-28 18:53:08 -0500

    Update: Pushing to save.

--- Square.php
+++ Square.php
@@ -5,27 +5,12 @@ require 'Rectangle.php';
 
  class Square extends Rectangle
 {
-	private $height;
-	private $width;
 
 	public function __construct($height)
 	{
 		$this->setHeight($height);
 		$this->setWidth($height);
 	}
-
-
-	// public function area()
-	// {
-	// 	return $this->getHeight() * $this->getWidth();
-
-	// } 
-
-	public function perimeter()
-	{
-		return ($this->getHeight() *2) + ($this->getWidth() * 2);
-
-	} 
 
 }
 
