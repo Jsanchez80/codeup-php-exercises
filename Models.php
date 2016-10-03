0504cc11ccf14aadf5b1eeaf2654aa4f66f79dfe
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-03 11:32:13 -0500

    Update: continuing to solve for problems outlined in exercise.

--- Models.php
+++ Models.php
@@ -27,40 +27,59 @@ abstract class Model
      *
      * This method should be called at the beginning of any function that needs to communicate with the database
      */
-    protected static function dbConnect()
-    {
-        if (!self::$dbc) {
-            // @TODO: Connect to database
-        }
-    }
+    // protected static function dbConnect()
+    // {
+    //     if (!self::$dbc) {
+    //         // @TODO: Connect to database
+    //         //  THIS OPERATION WILL ALLOW US TO CONNECT TO THE DATABASE WE SPECIFY SUCH AS THE EXAMPLE IN COMMENTS: 
+    //                                     // require 'DB File.php';
+    //                                     // define('DB_HOST', '127.0.0.1');
+    //                                     // define('DB_NAME', 'Models_db');
+    //                                     // define('DB_USER', 'vagrant');
+    //                                     // define('DB_PASS', 'vagrant');
+    //                                     // define("LIMIT", 10);
+    //         $dbc = new PDO('mysql:host='. DB_Host . ';dbname='. DB_Name, DB_USER, DB_PASS);
+    //             $dbc =setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     }
+    // }
 
-    /**
-     * Get a value from attributes based on its name
-     *
-     * @param string $name key for attributes array
-     *
-     * @return mixed|null value from the attributes array or null if it is undefined
-     */
-    public function __get($name)
-    {
-        // @TODO: Return the value from attributes for $name if it exists, else return null
-    }
+    // /**
+    //  * Get a value from attributes based on its name
+    //  *
+    //  * @param string $name key for attributes array
+    //  *
+    //  * @return mixed|null value from the attributes array or null if it is undefined
+    //  */
+    // public function __get($name)
+    // {
+    //     // @TODO: Return the value from attributes for $name if it exists, else return null
+    //     // COMPLETED WITH WITH THIS SET: 
+    //      if (array_key_exists($name, $this->attributes)) {
+    //         return $this->attributes[$name];
+    //     }
+    //         return null;
+    // }
 
-    /**
-     * Set a new value for a key in attributes
-     *
-     * @param string $name  key for attributes array
-     * @param mixed  $value value to be saved in attributes array
-     */
-    public function __set($name, $value)
-    {
-        // @TODO: Store name/value pair in attributes array
-    }
+    // /**
+    //  * Set a new value for a key in attributes
+    //  *
+    //  * @param string $name  key for attributes array
+    //  * @param mixed  $value value to be saved in attributes array
+    //  */
+    // public function __set($name, $value)
+    // {
+    //     // @TODO: Store name/value pair in attributes array
+    //     // COMPLETED WITH THE MENTIONED SET:
+    //     $this->attributes[$name] = $value;
+    // }
 
-    /** Store the object in the database */
+    // /** Store the object in the database */
     public function save()
     {
         // @TODO: Ensure there are values in the attributes array before attempting to save
+        // 
+       
+
 
         // @TODO: Call the proper database method: if the `id` is set this is an update, else it is a insert
     }
